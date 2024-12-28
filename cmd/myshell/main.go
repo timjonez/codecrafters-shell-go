4d43024e3ce42e477b90fa6973539080b9b6602c
Tim Jones <timjonez@protonmail.com>
2024-12-28 14:46:42 -0800

    handle echo commands, and refactor into switch statement

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,21 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+const SuccessCode = 0
+const ErrCode = 1
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprint(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		if command == "exit 0\n" {
-			os.Exit(0)
+
+		input = strings.TrimSpace(input)
+		commands := strings.Split(input, " ")
+		switch commands[0] {
+		case "exit":
+			code, err := strconv.Atoi(commands[1])
+			if err != nil {
+				os.Exit(ErrCode)
+			}
+			os.Exit(code)
+		case "echo":
+			fmt.Println(strings.Join(commands[1:], " "))
+		default:
+			fmt.Println(input[:len(input)-1] + ": command not found")
+
 		}
-		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 }
