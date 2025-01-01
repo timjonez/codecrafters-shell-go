d30117aa7f1664d35eb6d32271bb436c21782507
Tim Jones <timjonez@protonmail.com>
2024-12-31 22:38:02 -0800

    add missing print

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -72,6 +72,7 @@ func (i *Input) HandleOut(stdout, stderr []byte) {
 			if _, err := f.Write(stdout); err != nil {
 				fmt.Fprintf(os.Stderr, "Error writing to file: %v\n", err)
 			}
+			fmt.Fprint(os.Stderr, string(stderr))
 		}
 	} else {
 		if len(stderr) > 0 {
